34684cbb4cda1888e12367edfb8a551bd4216f53
Munish Khatri <109850452+munishkhatri720@users.noreply.github.com>
2023-11-13 06:09:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import discord
 from discord.ext import commands
+import os
+import dotenv import load_dotenv
+load_dotenv()
 
-bot = commands.Bot(command_prefix = "?")
+bot = commands.Bot(command_prefix = "?",intents = discord.Intents.all(),strip_after_prefix = True , case_insensitive= True)
+
+bot.run(os.getenv("TOKEN"))
